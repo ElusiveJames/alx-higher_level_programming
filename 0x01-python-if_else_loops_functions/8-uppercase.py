5f21e705edd0a8cbf380e5da01ee4eb6351f3e80
Elusive James <86646115+ElusiveJames@users.noreply.github.com>
2024-03-15 19:25:44 +0000

    to uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    str_2 = ''
+    for i in str:
+        if ord(i) >= 97 and ord(i) < 123:
+            str_2 += chr(ord(i) - 32)
+        else:
+            str_2 += i
+    print(str_2)
