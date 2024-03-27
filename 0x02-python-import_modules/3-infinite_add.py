ece0db6f11206d3d0cf2c8ac58d24a9841553ad8
Elusive James <86646115+ElusiveJames@users.noreply.github.com>
2024-03-27 14:33:29 +0000

    infinite add

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-import sys
-sum = 0
-count = 1
-while count <= len(sys.argv[1:]):
-    sum += int(sys.argv[count])
-    count += 1
-print(sum)
+if __name__ == "__main__":
+    import sys
+    sum = 0
+    count = 1
+    while count <= len(sys.argv[1:]):
+        sum += int(sys.argv[count])
+        count += 1
+    print(sum)
