a90109cb2ad27aedb4eb04584018539752d453cc
ElusiveJames <jmtokens@gmail.com>
2024-03-30 15:46:59 +0000

    only by 2

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+def divisible_by_2(my_list=[]):
+    if my_list:
+        new_list = []
+        for idx in range(len(my_list)):
+            if my_list[idx] % 2 == 0:
+                new_list.append(True)
+            else:
+                new_list.append(False)
+        return new_list
