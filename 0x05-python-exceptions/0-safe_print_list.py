eeecfab4a5abc061401636c6143ba6e583b3bed9
ElusiveJames <jmtokens@gmail.com>
2024-04-03 08:39:46 +0000

    safe prirint of an integer list

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        count = 0
+        for value in my_list[:x]:
+            print(f"{value}", end="")
+            count += 1
+    except IndexError:
+        print('IndexError')
+    return count
