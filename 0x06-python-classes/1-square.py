dc35253763905fd92f178ba73d5d420d9c0d951b
ElusiveJames <jmtokens@gmail.com>
2024-04-05 09:37:40 +0000

    square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" creating an empty class that define a square"""
+
+
+class Square:
+    """ define a square"""
+    def __init__(self, size):
+        """ initializing private  attribute size"""
+        self.__size = size
