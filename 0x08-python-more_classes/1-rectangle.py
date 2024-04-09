88c2448897aeb337aaef92037b387774bbc08d34
ElusiveJames <jmtokens@gmail.com>
2024-04-09 00:26:43 +0000

    Real defination of rectangle

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+""" creating a blueprin for rectangle"""
+
+
+class Rectangle:
+    """creating an empty class"""
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """show width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """" set value for width"""
+        if not isinstance(value, int):
+            raise TypeError('width must be an integer')
+        elif value < 0:
+            raise ValueError('width must be >= 0')
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """show the height"""
+        return sel.__height
+
+    @height.setter
+    def height(self, value):
+        """ setter method for height"""
+        if not isinstance(value, int):
+            raise TypeError('height must be an integer')
+        elif value < 0:
+            raise ValueError('height must be >= 0')
+        else:
+            self.__height = value
