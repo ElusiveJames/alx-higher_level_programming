6cbf85e3d4e6c93c2e4d239684c4d4673bbb8b78
ElusiveJames <86646115+ElusiveJames@users.noreply.github.com>
2024-07-12 12:11:36 +0300

    awesome God

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,21 +1,26 @@
 #include <stdio.h>
 #include "lists.h"
 
+/**
+ * check_cycle - checks if a singly linked list has a cycle
+ * @list: pointer to head of list
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
+
 int check_cycle(listint_t *list)
-{   
-    listint_t *ptr = NULL;
-    listint_t *head = list;
+{
+	listint_t *ptr = NULL;
+	listint_t *head = list;
 
-    if (list == NULL)
-        return (0);
-    ptr = head;
-    while (ptr->next != NULL)
-    {
-        if (ptr->next == head)
-            return (1);
-        ptr = ptr->next;
-    }
+	if (list == NULL)
+		return (0);
+	ptr = head;
+	while (ptr->next != NULL)
+	{
+		if (ptr->next == head)
+			return (1);
+		ptr = ptr->next;
+	}
 
-    return (0);
-    
+	return (0);
 }
