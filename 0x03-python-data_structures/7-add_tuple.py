14d052d4a0d4fd12ea02f62fa722e01c69735774
ElusiveJames <86646115+ElusiveJames@users.noreply.github.com>
2024-07-22 15:54:16 +0300

    more perspective

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,6 +2,8 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     if len(tuple_a) >= 1:
         new_a = tuple_a[0]
+    else:
+        new_a = 0
     if len(tuple_a) >= 2:
         new_b = tuple_a[1]
     else:
