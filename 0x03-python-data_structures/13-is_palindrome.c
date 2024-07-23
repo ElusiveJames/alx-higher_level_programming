5620fe4b95eab00bc265dc4a7ccaabac3f34f630
ElusiveJames <86646115+ElusiveJames@users.noreply.github.com>
2024-07-23 10:20:45 +0300

    optimized check

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,5 +1,6 @@
 #include "lists.h"
 #include <stddef.h>
+#include <stdlib.h>
 /**
 * is_palindrome - check if linkend list is palinddrome
 * @head: pointer to start of list
@@ -8,28 +9,43 @@
 int is_palindrome(listint_t **head)
 {
 	listint_t *temp = NULL;
-	int data[100];
+	int *elements = NULL;
 	int len, j, i;
 
 	if (head == NULL)
 		return (1);
 	temp = *head;
-	 i = 0;
+	i = 0;
 
+/* get length of list */
 	while (temp != NULL)
 	{
-		data[i] = temp->n;
+		temp = temp->next;
+		i++;
+	}
+/* allocate memory */
+	elements = malloc(sizeof(int) * i);
+	if (elements == NULL)
+		return (0);
+	temp = *head;
+	i = 0;
+	/* store list in array */
+	while (temp != NULL)
+	{
+		elements[i] = temp->n;
 		temp = temp->next;
 		i++;
 	}
 	len = i - 1;
 	j = 0;
+	/* check if palindrome */
 	while (j <= len)
 	{
-		if (data[j] != data[len])
+		if (elements[j] != elements[len])
 			return (0);
 		j++;
 		len--;
 	}
+	free(elements);
 	return (1);
 }
