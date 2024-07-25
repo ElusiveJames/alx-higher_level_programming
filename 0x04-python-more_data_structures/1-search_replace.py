7a7e829490e09e0157ae2206a8da00f23aba8102
ElusiveJames <86646115+ElusiveJames@users.noreply.github.com>
2024-07-25 09:20:26 +0300

    search and replace2

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,6 +2,7 @@
 
 def search_replace(my_list, search, replace):
     new_list = my_list[:]
-    idx = new_list.index(search)
-    new_list[idx] = replace
+    for idx in range(len(new_list)):
+        if new_list[idx] == search:
+            new_list[idx] = replace
     return new_list
