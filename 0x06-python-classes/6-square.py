da564a727272469b042840358f50fcd249681426
ElusiveJames <86646115+ElusiveJames@users.noreply.github.com>
2024-08-16 11:30:46 +0300

    coordinates of a square and capture error

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -44,7 +44,8 @@ class Square:
         if not isinstance(value, tuple) or len(value) != 2 or not\
                 all(isinstance(i, int) for i in value) or\
                 not all(i >= 0 for i in value):
-            raise TypeError('position must be a tuple of 2 positive integers')
+            print("position must be a tuple of 2 positive integers")
+             TypeError('position must be a tuple of 2 positive integers')
         self.__position = value
 
     def area(self):
