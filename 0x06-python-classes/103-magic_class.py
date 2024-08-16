6c9b6d6e5074b35e351fe3370827670aad55718e
ElusiveJames <86646115+ElusiveJames@users.noreply.github.com>
2024-08-16 09:06:41 +0300

    bytecode

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import math
+class MagicClass:
+    """ define a MagicClass"""
+    def __init__ (self, radius):
+        """ initializing private  attribute radius
+        Args:
+            self: self initialization
+            radius: object variable
+        Raises:
+            TypeError: if radius is not an integer or a float"""
+
+        if not isinstance(radius, (int, float)):
+            raise TypeError("radius must be a number")
+        self.__radius = radius
+    @property
+    def area(self):
+        """ get area of current square"""
+        return self.__radius ** 2 * math.pi
+
+    def circumference(self):
+        """ get circumference of current square"""
+        return 2 * math.pi* self.__radius
